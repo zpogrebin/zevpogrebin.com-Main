11693615575e1f57eed1192ba968242812e5eb9c
Zev Pogrebin <zpogrebin@putneyschool.org>
2020-01-22 19:56:45 -0500

    Fixed a dumb bug. Testing in prod! love it

--- sitestatus.js
+++ sitestatus.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	// {"title":" ","url":" ","imgsrc":" ","subtitle":" ","contenthtml":" "}, 
 
-	$.getJSON("http://zevpogrebin.com/data/projectdata.json", function(status) { //change url to fix
+	$.getJSON("http://zevpogrebin.com/data/sitestatus.json", function(status) { //change url to fix
 		//Console Log status
 		console.log(status.status);
 		//handle creating html
