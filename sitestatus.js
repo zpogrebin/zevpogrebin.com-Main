435dd424377e3338c23cb187256d10a83ff52aff
Zev Pogrebin <zpogrebin@putneyschool.org>
2020-01-22 19:49:03 -0500

    Last minute fixes to sitestatus.js

--- sitestatus.js
+++ sitestatus.js
@@ -3,11 +3,9 @@ $(document).ready(function() {
 	// {"title":" ","url":" ","imgsrc":" ","subtitle":" ","contenthtml":" "}, 
 
 	$.getJSON("http://zevpogrebin.com/data/projectdata.json", function(status) { //change url to fix
-		status = 
 		//Console Log status
 		console.log(status.status);
 		//handle creating html
 		("#status").append('<span style="color: '+status.color+';">'+status.status+'</span>');
 	});
-	
 });
